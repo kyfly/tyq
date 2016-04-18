9739dbd76ea7b42898a2668aaf763aac3a696c2f
lostwindtoku <849715057@qq.com>
2016-04-18 09:16:14 +0800

    搜索框修改。

--- public/javascripts/controllers/forumcontroller.js
+++ public/javascripts/controllers/forumcontroller.js
@@ -42,10 +42,6 @@ app.controller('ForumPublishCtrl', ['$scope', 'Ueditor', function ($scope, Uedit
 }]);
 
 app.controller('ForumTopicCtrl', ['$scope', function ($scope) {
-
-
-    //$scope.items = [];
-    //$scope.items = ['作者','看着','自己'];
     $scope.peopleInfo = [
         {
             imgSrc: "/lib/img/weixin.jpg",
@@ -81,31 +77,11 @@ app.controller('ForumTopicCtrl', ['$scope', function ($scope) {
             numberOfComment:"0"
         }
     ];
-
-    $(".button-collapse").sideNav();
-
-    //响应式图片
-    $(document).ready(function(){
-        $('.materialboxed').materialbox();
-    });
-
-    //下拉框
-    $(document).ready(function () {
-        $('select').material_select();
-    });
-
-    $('.dropdown-button').dropdown({
-            inDuration: 300,
-            outDuration: 225,
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            hover: true, // Activate on hover
-            gutter: 0, // Spacing from edge
-            belowOrigin: false // Displays dropdown below the button
-        }
-    );
-
-
-
+    ////下拉框
+    //$(document).ready(function () {
+    //    $('select').material_select();
+    //});
+    //
 }]);
 
 app.controller('ForumNoticeCtrl', ['$scope', function ($scope) {
