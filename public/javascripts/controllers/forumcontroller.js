99ac242cd3e184b8b3f13555761ca24bb87a36b3
LiC <1078054388@qq.com>
2016-04-23 16:01:48 +0800

    detail完成

--- public/javascripts/controllers/forumcontroller.js
+++ public/javascripts/controllers/forumcontroller.js
@@ -1,7 +1,7 @@
 app.controller('ForumUserlistCtrl', ['$scope', function ($scope) {
     $scope.userList = [{
         id: 1,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -9,7 +9,7 @@ app.controller('ForumUserlistCtrl', ['$scope', function ($scope) {
         replies: 30
     }, {
         id: 2,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -17,7 +17,7 @@ app.controller('ForumUserlistCtrl', ['$scope', function ($scope) {
         replies: 30
     }, {
         id: 3,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -29,7 +29,7 @@ app.controller('ForumUserlistCtrl', ['$scope', function ($scope) {
 app.controller('ForumBlacklistCtrl', ['$scope', function ($scope) {
     $scope.blacklist = [{
         id: 1,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -37,7 +37,7 @@ app.controller('ForumBlacklistCtrl', ['$scope', function ($scope) {
         replies: 30
     }, {
         id: 2,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -45,7 +45,7 @@ app.controller('ForumBlacklistCtrl', ['$scope', function ($scope) {
         replies: 30
     }, {
         id: 3,
-        headImgUrl: "/lib/img/weixin.jpg",
+        headImgUrl: "/img/weixin.jpg",
         name: "sm",
         score: 20,
         level: 20,
@@ -85,7 +85,7 @@ app.controller('ForumTopicCtrl', ['$scope', function ($scope) {
     $scope.peopleInfo = [
         {
             id: "name1",
-            imgSrc: "/lib/img/weixin.jpg",
+            imgSrc: "/img/weixin.jpg",
             name: "李畅",
             time: "2016.2.9 12时18分",
             title: "新人在此！！！",
@@ -97,19 +97,19 @@ app.controller('ForumTopicCtrl', ['$scope', function ($scope) {
         },
         {
             id: "name2",
-            imgSrc: "/lib/img/weixin.jpg",
+            imgSrc: "/img/weixin.jpg",
             name: "金老师",
             time: "2016.2.9 12时16分",
             title: "21岁 一型糖尿病",
             content: "刚确诊了，分手了 双重打击 有没有类似于群这样的 我加一个",
-            contentImg: "/lib/img/testImg1.png",
+            contentImg: "/img/testImg1.png",
             numberOfReader: "67",
             numberOfLike: "44",
             numberOfComment: "34"
         },
         {
             id: "name3",
-            imgSrc: "/lib/img/weixin.jpg",
+            imgSrc: "/img/weixin.jpg",
             name: "刘建东",
             time: "2016.2.9 10时18分",
             title: "测试测试",
