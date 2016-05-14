4617c31556663fb0fe2cd422e0675efd1f6a504c
liuJD <erchuochuo@163.com>
2016-05-14 12:39:23 +0800

    解决冲突

--- server/controllers/wechat.js
+++ server/controllers/wechat.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-//var wechatAPI = require('wechat-api');
-//var webot = require('weixin-robot');
-module.exports = function (webot) {
-    // webot.beforeReply(function (info, next) {
-    //     setTimeout(function () {
-    //         info.reply = [{
-    //                         "title": 'TITLETITLETITLETITLE',
-    //                         "picUrl": 'http://39.181.24.218/img/store.jpg',
-    //                         "description": "sadadasdasda"
-    //                     },{
-    //                         "title": 'TITLETITLETITLETITLE',
-    //                         "picUrl": 'http://39.181.24.218/img/store.jpg',
-    //                         "description": "sadadasdasda",
-    //                         "url": "www.baidu.com"
-    //                     }];
-    //         next();
-    //     }, 100)
-    // });
-    // webot.set(/.+/, function (info) {
-    //     return info.reply;
-    // });
-=======
 var wechatAPI = require('wechat-api');
 var webot = require('weixin-robot');
 var config = require('../config');
@@ -71,21 +48,11 @@ module.exports = function (webot) {
     webot.set(/.+/, function (info) {
         return info.reply;
     });
->>>>>>> ee12342e7b7f7b1089624ddbb3b67ff90c0d2306
 }
 /* 
     获取永久文章列表
  */
 module.exports.wechat = function (req, res, next) {
-<<<<<<< HEAD
-    // var api = new wechatAPI(req.app.get('wechat').appid, req.app.get('wechat').appsecret);
-    // var type = req.body.type || 'news';
-    // var offset = req.body.offset || 0;
-    // var count = req.body.count || 10;
-    // api.getMaterials(type, offset, count, function(err,result,gotten){
-    //     res.send(result);
-    // });
-=======
     var offset = req.body.offset || 0;
     var count = req.body.count || 10;
     getMaterials(offset, count)
@@ -171,5 +138,4 @@ function getMaterials (offset, count) {
             }
         });
     })
->>>>>>> ee12342e7b7f7b1089624ddbb3b67ff90c0d2306
 }
