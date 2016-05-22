93506bfe8a29f926bbad2f3e6ad5362326a02a1c
lostwindtoku <849715057@qq.com>
2016-05-23 07:51:02 +0800

    修改标签暂改

--- public/javascripts/controllers/forumcontroller.js
+++ public/javascripts/controllers/forumcontroller.js
@@ -112,6 +112,27 @@ app.controller('ForumTopicCtrl', ['$scope', 'Topic', 'User', function ($scope, T
         });
     };
 
+    $scope.changeTag = function () {
+        var thisElement = this;
+        Topic.updateById({
+            id : this.topic.id
+            //type :
+        },function(){
+
+        }
+        )
+
+    };
+    $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: true // Displays dropdown below the button
+        }
+    );
+
     $scope.changePage = function (page) {
         getTopic(page, $scope.search.content);
     };
